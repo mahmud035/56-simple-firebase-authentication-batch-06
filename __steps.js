b7272e9582f53bfaa1cf2441b999c3c8e6c473f1
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-13 15:28:00 +0600

    56-2 Create firebase project and Install Firebase SDK

--- __steps.js
+++ __steps.js
@@ -4,6 +4,9 @@
 2. create a new firebase project
 3. visit doc (go to docs)
 4. then go to: (Build => Authentication => Web => Get started)
-
+5. Register web app: (firebase project home => click web icon => give name and register)
+6. Install firebase for your project: npm install firebase
+7. DANGEROUS: get firebase configuration and put it in firebase.init.js file
+8. export app from firebase.init.js file
 
 */
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBG8FrxnKok_UCnj6xsplTEX6q2yYLPII8',
+  authDomain: 'simple-firebase-authenti-ead26.firebaseapp.com',
+  projectId: 'simple-firebase-authenti-ead26',
+  storageBucket: 'simple-firebase-authenti-ead26.appspot.com',
+  messagingSenderId: '810264603418',
+  appId: '1:810264603418:web:739cc5b08b9028c2835aa3',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
