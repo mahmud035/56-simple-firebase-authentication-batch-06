4eb048760ce5c26209afb9273c80c3c39652259c
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-13 14:57:06 +0600

    connected to github

---
+++ __steps.js
@@ -0,0 +1,9 @@
+/* 
+//* Steps for Creating firebase project and Install Firebase SDK
+1. visit: console.firebase.google.com
+2. create a new firebase project
+3. visit doc (go to docs)
+4. then go to: (Build => Authentication => Web => Get started)
+
+
+*/
